bf902fb9cc1d153caf397934425845e7882a1674
BedirhanTheDude <bedirhan190734gfb@gmail.com>
2026-04-18 01:56:10 +0300

    added obj caching for faster loading

--- Engine/src/renderer/ObjLoader.h
+++ Engine/src/renderer/ObjLoader.h
@@ -3,8 +3,14 @@
 #include <renderer/Mesh.h>
 #include <memory>
 #include <string>
+#include <vector>
+
+struct Vertex;
 
 class ObjLoader {
 public:
-	static std::shared_ptr<Mesh> load(const std::string& filePath);
+	static std::shared_ptr<Mesh> load(const std::string& filePath, bool forceLoadNew = false);
+private:
+	static void saveCache(const std::string& objName, const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices);
+	static bool loadCache(const std::string& objName, std::vector<Vertex>& vertices, std::vector<unsigned int>& indices);
 };
