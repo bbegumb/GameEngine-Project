f871d989e7301e38a26826a245eac7740431c808
bbegumb <bbicakci23@ku.edu.tr>
2026-05-06 02:23:29 +0300

    fixed gui for adding components

--- Sandbox/src/gui/EditorLayer.h
+++ Sandbox/src/gui/EditorLayer.h
@@ -3,7 +3,6 @@
 #include "imgui.h"
 
 class Scene;
-class Entity;
 
 class EditorLayer {
 public:
@@ -11,7 +10,7 @@ public:
     void SetViewportTexture(unsigned int textureID);
 
     void OnUIRender();
-    ImVec2 GetViewportSize() const { return m_ViewportSize; }
+    ImVec2 GetViewportSize() const { return viewportSize; }
 
 private:
     void ShowDockspace();
@@ -23,15 +22,12 @@ private:
     void ShowViewportPanel();
 
 private:
-    Scene* m_Scene = nullptr;
-    Entity* m_SelectedEntity = nullptr;
+    unsigned int viewportTexture = 0;
+    ImVec2 viewportSize = ImVec2(0.0f, 0.0f);
 
-    unsigned int m_ViewportTexture = 0;
-    ImVec2 m_ViewportSize = ImVec2(0.0f, 0.0f);
-
-    bool m_ShowStats = true;
-    bool m_ShowHierarchy = true;
-    bool m_ShowInspector = true;
-    bool m_ShowConsole = true;
-    bool m_ShowViewport = true;
+    bool showStats = true;
+    bool showHierarchy = true;
+    bool showInspector = true;
+    bool showConsole = true;
+    bool showViewport = true;
 };
