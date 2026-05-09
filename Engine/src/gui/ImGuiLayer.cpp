1b5286edf2c0be0a66ee96ce951eda2242d14414
bbegumb <bbicakci23@ku.edu.tr>
2026-05-10 02:05:34 +0300

    a

---
+++ Engine/src/gui/ImGuiLayer.cpp
@@ -0,0 +1,61 @@
+#include "ImGuiLayer.h"
+
+#define GL_SILENCE_DEPRECATION
+#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+void ImGuiLayer::Init(GLFWwindow* window) {
+    m_Window = window;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowRounding = 5.0f;
+    style.FrameRounding = 4.0f;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+
+#ifdef __APPLE__
+    ImGui_ImplOpenGL3_Init("#version 150");
+#else
+    ImGui_ImplOpenGL3_Init("#version 330");
+#endif
+}
+
+void ImGuiLayer::Begin() {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void ImGuiLayer::End() {
+    ImGui::Render();
+
+    int displayWidth = 0;
+    int displayHeight = 0;
+    glfwGetFramebufferSize(m_Window, &displayWidth, &displayHeight);
+    glViewport(0, 0, displayWidth, displayHeight);
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void ImGuiLayer::Shutdown() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
